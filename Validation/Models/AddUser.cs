633a34e18c6ec4e74ca2da99c5311013662febf9
Metin Akanay KIRDAR <metinakanay@hotmail.com>
2024-04-06 10:23:34 +0300

    .

--- Validation/Models/AddUser.cs
+++ Validation/Models/AddUser.cs
@@ -12,21 +12,21 @@ namespace Validation.Models
 
         [Display(Name = "İsim")]
         [StringLength(20,MinimumLength = 3, ErrorMessage ="Ad alanı belirlenen kurallar dahilinde değil.")]
-        [Required(ErrorMessage = "Ad alanı boş geçilemez.")]
+        [Required(ErrorMessage = "Ad alanı boş bırakılamaz.")]
         public string Name { get; set; }
 
         [Display(Name = "Soyisim")]
         [StringLength(60,MinimumLength = 2, ErrorMessage ="Soy ad alanı belirlenen kurallar dahilinde değil.")]
-        [Required(ErrorMessage = "Soy ad alanı boş geçilemez.")]
+        [Required(ErrorMessage = "Soy ad alanı boş bırakılamaz.")]
         public string LastName { get; set; }
 
         [Display(Name = "E-posta")]
-        [Required(ErrorMessage = "E-posta alanı boş geçilemez.")]
+        [Required(ErrorMessage = "E-posta alanı boş bırakılamaz.")]
         [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", ErrorMessage = "Geçerli bir e-posta adresi giriniz.")]
         public int Email { get; set; }
 
         [Display(Name = "Yaş")]
-        [Required(ErrorMessage = "Yaş alanı boş geçilemez.")]
+        [Required(ErrorMessage = "Yaş alanı boş bırakılamaz.")]
         [Range(18, 65, ErrorMessage = "Yaş aralığı 18-65 olmalıdır.")]
         public int Age { get; set; }
 
